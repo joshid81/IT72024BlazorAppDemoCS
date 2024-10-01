b11f8ea0af94dfcedcc28e9b0cd923f72bf0978b
joshid81 <dhaval.joshi81@gmail.com>
2024-10-01 21:49:38 +0530

    C1

---
+++ DataClasses/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace IT72024BlazorAppDemoCS.DataClasses
+{
+    //public class Product
+    //{
+    //    public int ProductID;
+    //    public string ProductName;
+
+    //}
+
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; } = null!;
+
+        public int Rate { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
